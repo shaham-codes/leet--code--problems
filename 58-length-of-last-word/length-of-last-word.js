cfbb39b97cd8a9b97b47d1b6c7a9d80a4a1db1dd
shaham <shahamsham05@gmail.com>
2026-09-07 10:51:12 +0530

    Time: 0 ms (100.00%) | Memory: 53.6 MB (56.92%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+
+
+var lengthOfLastWord = function(s) {
+     let words = s.trim().split(" ");
+      return words[words.length - 1].length;
+       };
